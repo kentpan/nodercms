75612f90e8001d36646adbcfa0b309cf3ea40e99
panjian01 <panjian01@baidu.com>
2021-03-24 15:24:53 +0800

    添加google收入，多域名独立配置

--- core/services/site-info.service.js
+++ core/services/site-info.service.js
@@ -2,22 +2,27 @@ var logger = require('../../lib/logger.lib');
 var cache = require('../../lib/cache.lib');
 var optionsModel = require('../models/options.model');
 
+
+function getDomain (req) {
+  var domain = (req.query && req.query.host) || req.domain || req.headers.host || 'www.yoozworld.co';
+  domain = domain.indexOf('127.0.0.1') > -1 ? 'www.yoozworld.co' : domain;
+  if (!/^www\./.test(domain)) {
+    domain = 'www.' + domain;
+  }
+  return domain;
+}
 /**
  * 网站信息
  * @param {Function} callback
  */
 exports.get = function (req, callback) {
-  var siteInfoCache = cache.get('siteInfo');
-  var domain = req.query && req.query.host || req.host;
-  domain = domain.indexOf('127.0.0.1') > -1 ? 'www.yoozworld.co' : domain;
-  if (!/^www\./.test(domain)) {
-    domain = 'www.' + domain;
-  }
-  if (siteInfoCache && siteInfoCache.domain === domain) {
-    console.log('host:', domain, ' siteInfoCache:', siteInfoCache);
+  var domain = getDomain(req);
+  var siteInfoCache = cache.get('siteInfo_' + domain);
+  if (siteInfoCache) {
+    console.log('host:', domain, 'siteInfoCache.domain:', siteInfoCache.domain, ' siteInfoCache:', siteInfoCache);
     callback(null, siteInfoCache);
   } else {
-    console.log('host:', domain, ' url:', req.url, ' query:', req.query, ' params:', req.params);
+    console.log('host:', domain, ' query:', req.query, ' params:', req.params);
     optionsModel.findOne({ name: 'siteInfo', $or: [ { domain: domain }, { 'value.domain': domain } ] }, function (err, siteInfo) {
       if (err) {
         err.type = 'database';
@@ -25,7 +30,7 @@ exports.get = function (req, callback) {
       }
 
       if (siteInfo) {
-        cache.set('siteInfo', siteInfo.value, 1000 * 60 * 60 * 24 * 30);
+        cache.set('siteInfo_' + domain, siteInfo.value, 1000 * 60 * 60 * 24 * 30);
 
         callback(null, siteInfo.value);
       } else {
@@ -42,24 +47,25 @@ exports.get = function (req, callback) {
  * @param {Function} callback
  */
 exports.save = function (options, callback) {
-  optionsModel.findOne({ name: 'siteInfo', $or: [ { domain: options.data.domain }, { 'value.domain': options.data.domain } ] }, function (err, siteInfo) {
+  var domain = getDomain(options.data);
+  optionsModel.findOne({ name: 'siteInfo', $or: [ { domain: domain }, { 'value.domain': domain } ] }, function (err, siteInfo) {
     if (err) {
       err.type = 'database';
       callback(err);
     }
     // 如果当前域名配置文件已经存在，则直接更新
     if (siteInfo && siteInfo.toString() !== '{}') {
-      console.log('域名配置文件已存在，直接更新 =>', options.data.domain);
-      optionsModel.findOneAndUpdate({ name: 'siteInfo', 'value.domain': options.data.domain }, {
+      console.log('域名配置文件已存在，直接更新 =>', domain);
+      optionsModel.findOneAndUpdate({ name: 'siteInfo', 'value.domain': domain }, {
         value: options.data,
-        domain: options.data.domain || 'www.yoozworld.co'
+        domain: domain || 'www.yoozworld.co'
       }, { runValidators: true }, function (err) {
         if (err) {
           err.type = 'database';
           callback(err);
         }
 
-        cache.del('siteInfo');
+        cache.del('siteInfo_' + domain);
 
         callback(null);
       });
@@ -72,7 +78,7 @@ exports.save = function (options, callback) {
           callback(err);
         }
 
-        cache.del('siteInfo');
+        cache.del('siteInfo_' + domain);
 
         callback(null, siteInfo);
       });
@@ -88,7 +94,7 @@ exports.save = function (options, callback) {
   //     callback(err);
   //   }
 
-  //   cache.del('siteInfo');
+  //   cache.del('siteInfo_' + domain);
 
   //   callback(null);
   // });
